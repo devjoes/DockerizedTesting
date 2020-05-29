c1c9ba4556a897a87a8bb175ae222ce5f95d9876
Joe Shearn <joseph.shearn@sainsburys.co.uk>
2020-05-29 14:25:33 +0100

    made it kill any old containers on next run

--- DockerizedTesting/Containers/ContainerActions.cs
+++ DockerizedTesting/Containers/ContainerActions.cs
@@ -30,38 +30,49 @@ namespace DockerizedTesting.Containers
         public int GetPort()=>
             this.uniquePortProvider.GetPort();
 
-        public Task KillZombieContainersBoundToPorts(int[] ports)
+        public async Task KillZombieContainersBoundToPorts(int[] ports)
         {
-            throw new NotImplementedException();
+            using (var dockerClient = this.dockerClientProvider.GetDockerClient())
+            {
+                var containers = await dockerClient.Containers.ListContainersAsync(new ContainersListParameters
+                {
+                    All = true
+                });
+                var toKill = containers.Where(c => c.Ports.Any(p => ports.Contains(p.PublicPort)));
+                await Task.WhenAll(toKill.Select(c =>
+                    dockerClient.Containers.StopContainerAsync(c.ID, new ContainerStopParameters { WaitBeforeKillSeconds = 1 })));
+            }
         }
 
         public async Task<string> StartContainer(CreateContainerParameters containerParameters, IDockerImageProvider imageProvider, string containerName)
         {
-            var dockerClient = this.dockerClientProvider.GetDockerClient();
-            containerParameters.Image = await imageProvider.GetImage(dockerClient);
-
-            var containers = await dockerClient.Containers.ListContainersAsync(new ContainersListParameters { All = true });
-            var existingContainer = containers.SingleOrDefault(c => c.Names.Contains("/" + containerName));
-
-            string containerId = null;
-            if (existingContainer != null)
+            using (var dockerClient = this.dockerClientProvider.GetDockerClient())
             {
-                containerId = existingContainer.ID;
-                if (existingContainer.State == "running")
+                containerParameters.Image = await imageProvider.GetImage(dockerClient);
+
+                var containers = await dockerClient.Containers.ListContainersAsync(new ContainersListParameters { All = true });
+                var existingContainer = containers.SingleOrDefault(c => c.Names.Contains("/" + containerName));
+
+                string containerId = null;
+                if (existingContainer != null)
                 {
-                    return containerId;
+                    containerId = existingContainer.ID;
+                    if (existingContainer.State == "running")
+                    {
+                        return containerId;
+                    }
                 }
-            }
-            if (containerId == null)
-            {
-                containerParameters.Name = containerName;
-                var container = await dockerClient.Containers.CreateContainerAsync(containerParameters);
-                containerId = container.ID;
-            }
+                if (containerId == null)
+                {
+                    containerParameters.Name = containerName;
+                    var container = await dockerClient.Containers.CreateContainerAsync(containerParameters);
+                    containerId = container.ID;
+                }
 
-            await dockerClient.Containers.StartContainerAsync(containerId, new ContainerStartParameters());
-            ContainerHost.Instance.ContainerIds.TryAdd(containerId, dockerClientProvider.DockerUri);
-            return containerId;
+                await dockerClient.Containers.StartContainerAsync(containerId, new ContainerStartParameters());
+                ContainerHost.Instance.ContainerIds.TryAdd(containerId, dockerClientProvider.DockerUri);
+                return containerId;
+            }
         }
 
         public void StopContainer(string containerId)
